5e27c09c46b347f1b84a33908969ec0d73c8808c
Poonama30N <65127138+Poonama30N@users.noreply.github.com>
2020-06-29 18:33:53 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -2,13 +2,14 @@
 data<-read.table("household_power_consumption.txt",sep=';',header = TRUE)
 
 reqdata<-subset(data,Date=="1/2/2007"|Date=="2/2/2007")
+reqdata$Date <- as.Date(reqdata$Date, format="%d/%m/%Y")
 
 #Plot1
 str(reqdata)
-globalActivePower <- as.numeric(reqdata$Global_active_power)
-
 png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", 
+
+hist(as.numeric(as.character(reqdata$Global_active_power)), col="red", 
      main="Global Active Power", 
      xlab="Global Active Power (kilowatts)")
+
 dev.off()
